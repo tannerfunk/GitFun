ccef67bf7cf5ad7eda9375be92ed5d81fbee7b16
Tanner Funk <tannerfunk4@gmail.com>
2023-01-29 09:37:11 -0700

    Cleaned up the project.

--- GitFun/Controllers/HomeController.cs
+++ GitFun/Controllers/HomeController.cs
@@ -11,11 +11,9 @@ namespace GitFun.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController()
         {
-            _logger = logger;
         }
 
         public IActionResult Index()
@@ -23,15 +21,5 @@ namespace GitFun.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
